43a35a0d2b47c34ebbe282ba28d260f9005472f3
Jakub1888 <krasnan.jk@gmail.com>
2022-11-27 08:17:05 +0100

    api skeleton

---
+++ apps/api/src/app/server.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { NextFunction, Request, Response } from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+import Logging from './library/logging';
+import * as path from 'path';
+
+const router = express();
+
+//** Use static files */
+router.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+/** Connect to Mongo */
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(config.mongo.url, { retryWrites: true, w: 'majority' });
+        Logging.info('connected');
+        StartServer();
+    } catch (error) {
+        Logging.error('Unable to connect');
+        Logging.error(error);
+    }
+};
+
+/** Start server only if Mongo connects */
+const StartServer = () => {
+    router.use((req: Request, res: Response, next: NextFunction) => {
+        /** Log the request */
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status [${res.statusCode}]`);
+        });
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    /** Api Rules */
+    router.use((req: Request, res: Response, next: NextFunction) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'Options') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /** Routes */
+
+    /** Healthcheck */
+    router.get('/ping', (req: Request, res: Response, next: NextFunction) => res.status(200).json({ message: 'pong' }));
+
+    /** Error handling */
+    router.use((req: Request, res: Response, next: NextFunction) => {
+        const error = new Error('not found');
+        Logging.error(error);
+
+        return res.status(404).json({ message: error.message });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`));
+};
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,30 +1,11 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-require('dotenv').config();
-import express from 'express';
-import * as path from 'path';
-import cors = require('cors');
-const connectDB = require('./app/utils/connect');
-
-const app = express();
-
-app.use(cors());
-
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-    res.send({ message: 'Welcome to api!' });
-});
-
-const port = process.env.port || 3333;
+import { connectDB } from './app/server';
+import Logging from './app/library/logging';
 
 const start = async () => {
     try {
-        // await connectDB(process.env.MONGO_URI);
-        app.listen(port, () => {
-            return console.log(`Listening at http://localhost:${port}/api`);
-        });
+        await connectDB();
     } catch (error) {
-        console.log(error);
+        Logging.error(error);
     }
 };
 
